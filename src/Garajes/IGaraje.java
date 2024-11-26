6ea13251eb52b5551dca23fa5c340cba0165f637
admin <admin@DESKTOP-1K2DAMO>
2024-11-26 16:55:16 -0500

    PROYECTO POO DEFINITIVO 2

---
+++ src/Garajes/IGaraje.java
@@ -0,0 +1,17 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Garajes;
+
+import Vehiculos.Vehiculo;
+
+/**
+ *
+ * @author Luis
+ */
+public interface IGaraje {
+  double calcularIngresos();
+  int calcularOcupacionPorTipoVehiculo(Vehiculo v);
+}
